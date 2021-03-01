87b9f1cf3a15ff921a758f8cc1ffdad92b4503b1
klk645445 <james.kway@yahoo.com.sg>
2021-03-02 00:05:57 +0800

    Implemented before / after parameters
    Implemented before/after optional parameter for the analyze and analyzechannel commands

--- analyze.py
+++ analyze.py
@@ -1,6 +1,7 @@
 from discord import Embed, User, TextChannel
 from discord.ext import commands
 from discord.ext.menus import ListPageSource, MenuPages
+from datetime import datetime
 
 
 class ChatsMenu(ListPageSource):  # just shows number of messages, no functionality to check before or after some time
@@ -39,7 +40,7 @@ class ChannelMenu(ListPageSource):  # just shows number of messages, no function
 
         position = 1
 
-        for k, v in entries:  # k-> channel id, v -> number of messages
+        for k, v in entries:  # k->  user, v -> number of messages sent in channel
             user = menu.bot.get_user(k)
             embed.add_field(name="Position", value=position, inline=True)
             embed.add_field(name="User", value=f'{user.name}#{user.discriminator}', inline=True)
@@ -48,37 +49,115 @@ class ChannelMenu(ListPageSource):  # just shows number of messages, no function
 
         return embed
 
-
 class analyze(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name="analyze")
-    async def _analyze(self, ctx, user: User):
-        async with self.bot.pool.acquire() as con:
-            result = await con.fetch("select * from chats where userid = $1", user.id)
+    @staticmethod
+    async def _convert_to_datetime(data: str):
+        words = data.split()
+        before = None
+        after = None
 
-            x = dict(result[0])
+        for i, word in enumerate(words):
+            if word.lower() == "-b":
+                try:
+                    before = datetime.strptime(words[i + 1], "%Y-%m-%d")
+                except Exception as e:
+                    print(e)
+
+            if word.lower() == "-a":
+                try:
+                    after = datetime.strptime(words[i + 1], "%Y-%m-%d")
+                except Exception as e:
+                    print(e)
+
+        return before, after
+
+    @commands.command(name="analyze")
+    async def _analyze(self, ctx, user: User, *, time=None):
+        async with self.bot.pool.acquire() as con:
+            results = []
+
+            # if a time period isn't specified, get the default results
+            if time is None:
+                results = await con.fetch(f"""SELECT channel_id, COUNT(*)
+                                              FROM chats
+                                              WHERE userid = {user.id}
+                                              GROUP BY channel_id
+                                              ORDER BY COUNT(*) DESC""")
+
+            else:
+                before, after = await self._convert_to_datetime(time)
+
+                if before is None:
+                    before = datetime.utcnow()
+
+                if after is None:
+                    results = await con.fetch(f"""SELECT channel_id, COUNT(*)
+                                                  FROM chats
+                                                  WHERE userid = {user.id}
+                                                  AND created_at < $1
+                                                  GROUP BY channel_id
+                                                  ORDER BY COUNT(*) DESC""", before)
+
+                else:
+                    results = await con.fetch(f"""SELECT channel_id, COUNT(*)
+                                                      FROM chats
+                                                      WHERE userid = {user.id}
+                                                      AND (created_at BETWEEN $2 AND $1)
+                                                      GROUP BY channel_id
+                                                      ORDER BY COUNT(*) DESC""", before, after)
+
+            if not results:
+                return await ctx.reply("No records found.")
+
             data = []
-            for k, v in x.items():
-                if v:  # if the array is empty, its not appended to the list
-                    if k == "userid":  # ignores the userid column
-                        continue
-                    data.append((int(k[1:]), len(v)))
+            for result in results:
+                data.append((int(result['channel_id']), result['count']))
 
             pages = MenuPages(source=ChatsMenu(data), clear_reactions_after=True)
             await pages.start(ctx)
 
     @commands.command(name="analyzechannel")
-    async def _analyzechannel(self, ctx, channel: TextChannel):
+    async def _analyzechannel(self, ctx, channel: TextChannel, *, time=None):
         async with self.bot.pool.acquire() as con:
+            results = []
             # select the channel id's along with the user ids
-            result = await con.fetch(f"SELECT _{channel.id}, userid FROM chats ORDER BY array_length(_{channel.id}, 1) desc")
+            if time is None:
+                results = await con.fetch(f"""SELECT userid, COUNT(*)
+                                              FROM chats
+                                              WHERE channel_id = {channel.id}
+                                              GROUP BY userid
+                                              ORDER BY COUNT(*) DESC""")
+
+            else:
+                before, after = await self._convert_to_datetime(time)
+
+                if before is None:
+                    before = datetime.utcnow()
+
+                if after is None:
+                    results = await con.fetch(f"""SELECT userid, COUNT(*)
+                                                  FROM chats
+                                                  WHERE channel_id = {channel.id}
+                                                  AND created_at < $1
+                                                  GROUP BY userid
+                                                  ORDER BY COUNT(*) DESC""", before)
+
+                else:
+                    results = await con.fetch(f"""SELECT userid, COUNT(*)
+                                                      FROM chats
+                                                      WHERE channel_id = {channel.id}
+                                                      AND (created_at BETWEEN $2 AND $1)
+                                                      GROUP BY userid
+                                                      ORDER BY COUNT(*) DESC""", before, after)
+
             data = []
             # for each result in the array, append to the data list the user id and how many messages they send
             # (userid, int)
-            for user in result:
-                data.append((int(user['userid']), len(user[f'_{channel.id}'])))
+            for result in results:
+                data.append((int(result['userid']), result['count']))
 
             pages = MenuPages(source=ChannelMenu(data), clear_reactions_after=True)
 
